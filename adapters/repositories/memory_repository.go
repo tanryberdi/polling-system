013d97d8e10c791aa4eb5f6bc29182202a5fda2b
Tanryberdi <tanryberdi@gmail.com>
2024-07-24 15:55:20 +0500

    adapters repositories added

---
+++ adapters/repositories/memory_repository.go
@@ -0,0 +1,67 @@
+package repositories
+
+import (
+	"fmt"
+	"sync"
+
+	"polling-system/domain"
+)
+
+type MemoryRepository struct {
+	polls     map[string]*domain.Poll
+	votes     map[string]map[string]int
+	pollMutex sync.RWMutex
+	voteMutex sync.RWMutex
+}
+
+func NewMemoryRepository() *MemoryRepository {
+	return &MemoryRepository{
+		polls: make(map[string]*domain.Poll),
+		votes: make(map[string]map[string]int),
+	}
+}
+
+func (r *MemoryRepository) CreatePoll(poll domain.Poll) error {
+	r.pollMutex.Lock()
+	defer r.pollMutex.Unlock()
+	r.polls[poll.ID] = &poll
+	r.votes[poll.ID] = make(map[string]int)
+	return nil
+}
+
+func (r *MemoryRepository) GetPoll(id string) (domain.Poll, error) {
+	r.pollMutex.RLock()
+	defer r.pollMutex.RUnlock()
+	poll, ok := r.polls[id]
+	if !ok {
+		return domain.Poll{}, fmt.Errorf("poll not found")
+	}
+	return *poll, nil
+}
+
+func (r *MemoryRepository) Vote(vote domain.Vote) error {
+	r.voteMutex.Lock()
+	defer r.voteMutex.Unlock()
+	if _, ok := r.votes[vote.PollID]; !ok {
+		return fmt.Errorf("poll not found")
+	}
+	r.votes[vote.PollID][vote.Option]++
+	return nil
+}
+
+func (r *MemoryRepository) GetResults(pollID string) (domain.PollResult, error) {
+	r.pollMutex.RLock()
+	defer r.pollMutex.RUnlock()
+	r.voteMutex.RLock()
+	defer r.voteMutex.RUnlock()
+
+	poll, ok := r.polls[pollID]
+	if !ok {
+		return domain.PollResult{}, fmt.Errorf("poll not found")
+	}
+
+	return domain.PollResult{
+		Poll:    *poll,
+		Results: r.votes[pollID],
+	}, nil
+}
